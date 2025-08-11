190f607a9f8b923cf6acdb57e2bfea2824f89860
Patrick Ineichen <github@frokonet.ch>
2025-08-11 10:54:14 +0200

    feat(nvim): align with base dotfiles repo

--- nvim/lua/plugins/coding/mason.lua
+++ nvim/lua/plugins/coding/mason.lua
@@ -3,14 +3,7 @@ return {
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     event = 'VeryLazy',
     opts = function(_, opts)
-      local tools = {
-        'lua-language-server',
-        'stylua',
-        'prettier',
-        'jsonlint',
-        'yamllint',
-        'tombi',
-      }
+      local tools = { 'lua-language-server', 'stylua', 'prettier', 'jsonlint' }
       opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, tools)
     end,
     config = function(_, opts)
